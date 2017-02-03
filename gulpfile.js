63d9e86ebe48eacf41a3e4556582451c87ba06b9
Aziz Dzhetygenov <wazzzup916@gmail.com>
2017-02-03 18:51:46 +0500

    Fixed some

--- gulpfile.js
+++ gulpfile.js
@@ -13,67 +13,98 @@ var gulp = require("gulp"),
 		notify = require("gulp-notify"),
 		useref = require("gulp-useref"),
 		gulpIf = require("gulp-if"),
+		newer = require("gulp-newer"),
 		browserSync = require("browser-sync").create();
 
-/*== Launch the server ==*/
-gulp.task('serve', function() {
-	browserSync.init({
-		server: {
-			baseDir: "app/"	
-		}
-	});
+var tasks = {
+	/**
+	 * Serving localhost
+	 */
+	
+	serving: function() {
+		browserSync.init({
+			server: {
+				baseDir: "app/"	
+			}
+		});
 		gulp.watch('app/**/*.*').on('change', browserSync.reload)
-});
+	},
 
-/*== Deleting directories ==*/
-gulp.task('clean', function(){
-	return del('app');
-});
+	/**
+	 * Compile styles etc.
+	 */
+	
+	optimizingCss: function() {
+		return gulp.src('dist/sass/**/*.sass')
+			.pipe(sass().on('error', sass.logError))
+			.pipe(autoprefixer())
+			.pipe(csso())
+			.pipe(rename({suffix: ".min"}))
+			.pipe(notify())
+			.pipe(gulp.dest('app/style/'))
+			.pipe(browserSync.stream());
+	},
 
-/*== Compiling styles ==*/
-gulp.task('styles', function () {
-	return gulp.src('dist/sass/**/*.sass')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer())
-		.pipe(csso())
-		.pipe(rename({suffix: ".min"}))
-		.pipe(notify())
-		.pipe(gulp.dest('app/style/'))
-		.pipe(browserSync.stream());
-});
-
-/*== Compiling pug ==*/
-gulp.task('views', function(){
-	return gulp.src('dist/pug/index.pug')
-		.pipe(pug({
+	/**
+	 * Compile views(pug.js).
+	 */
+	
+	views: function() {
+		return gulp.src('dist/pug/index.pug')
+			.pipe(pug({
 				pretty: true
-		}))
-		.pipe(notify())
-		.pipe(gulp.dest('app/'))
-		.pipe(browserSync.stream());
-});
+			}))
+			.pipe(useref())
+			.pipe(gulpIf('*.js', uglify()))
+			.pipe(notify())
+			.pipe(gulp.dest('app/'))
+			.pipe(browserSync.stream());
+	},
 
-/*== Minifying images ==*/
-gulp.task('images', function(){
-	return gulp.src('dist/images/**/*.*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('app/img/'))
-})
+	/**
+	 * Optimizing images
+	 */
 
-/*==Scripts==*/
-gulp.task("scripts", function(){
-	return gulp.src("dist/js/*.js")
-		.pipe(uglify())
-		.pipe(rename({suffix: ".min"}))
-		.pipe(gulp.dest("app/js"));
-})
+	optimizeImages: function() {
+		return gulp.src('dist/images/**/*.*')
+			.pipe(newer("app/img/"))
+			.pipe(imagemin())
+			.pipe(gulp.dest('app/img/'))
+	},
+
+	/**
+	 * Minifying scripts
+	 */
+	
+	uglifyingJs: function() {
+		return gulp.src("dist/js/*.js")
+			.pipe(uglify())
+			.pipe(rename({suffix: ".min"}))
+			.pipe(gulp.dest("app/js"));
+	},
+
+	/**
+	 * Watcher
+	 */
+	
+	watching: function() {
+		gulp.watch("dist/sass/**/*.sass", ["styles"]);
+		gulp.watch("dist/pug/**/*.pug", ["views"]);
+		gulp.watch("dist/images/**/*.*", ["images"]);
+		gulp.watch("dist/js/**/*.js", ["scripts"])
+	}
+};
 
 
-gulp.task('watch', function(){
-	gulp.watch("dist/sass/**/*.sass", ["styles"]);
-	gulp.watch("dist/pug/**/*.pug", ["views"]);
-	gulp.watch("dist/images/**/*.*", ["images"]);
-	gulp.watch("dist/js/**/*.js", ["scripts"])
-})
+/**
+ * TASKS ARE BEING CALLED
+ */
+gulp.task('serve', tasks.serving());
+gulp.task('styles', tasks.optimizingCss);
+gulp.task('views', tasks.views);
+gulp.task('images', tasks.optimizeImages);
+gulp.task("scripts", tasks.uglifyingJs);
+gulp.task('watch', tasks.watching);
 
-gulp.task("default", [ "serve", "watch"])
+/*Default task*/
+gulp.task("default", ["serve", "watch"]);
