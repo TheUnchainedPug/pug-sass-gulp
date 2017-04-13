f3f8222c4329ff87cebf16b8d26676e7cc64d391
Aziz Dzhetygenov <wazzzup916@gmail.com>
2017-04-14 01:20:58 +0500

    Organized gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,31 @@
 "use strict";
 
 const gulp = require("gulp");
-const sass = require("gulp-sass");
-const plumber = require("gulp-plumber");
-const csso = require("gulp-csso");
-const rename = require("gulp-rename");
-const uglify = require("gulp-uglify");
-const imagemin = require("gulp-imagemin");
-const autoprefixer = require("gulp-autoprefixer");
-const sourcemaps = require("gulp-sourcemaps");
-const notify = require("gulp-notify");
-const browserSync = require("browser-sync").create();
-const pug = require("gulp-pug");
-const useref = require("gulp-useref");
-const gulpIf = require("gulp-if");
-const del = require("del");
-const newer = require("gulp-newer");
-const cached = require("gulp-cached");
-const debug = require("gulp-debug");
-const bs = require("browser-sync").create();
+const config = require('./tasks/config.js');
+const reqDir = require('require-dir');
+reqDir('./tasks', { recurse: true });
 
-
-gulp.task("clean", function(){
-	return(del('build'));
-});
-
-gulp.task("compile:styles", function() {
-	return gulp.src("source/sass/application.sass")
-		.pipe(plumber())
-		.pipe(sass()).on("error", notify.onError(function(err){
-			return {
-				title: "Styles",
-				message: err.message
-			}
-		}))
-		.pipe(debug({
-			title: "Styles are compiled: "
-		}))
-		.pipe(autoprefixer({
-            browsers: ['last 4 versions'],
-        }))	
-		.pipe(csso())
-		.pipe(rename({
-			suffix: ".min"
-		}))
-		.pipe(notify())
-		.pipe(gulp.dest("build/style"))
-});
-
-gulp.task("compile:views", function() {
-	return gulp.src("source/pug/*.pug")
-		.pipe(plumber())
-		.pipe(pug({
-			pretty: true
-		}))
-		.on("error", notify.onError(function(err){
-			return {
-				title: "Views",
-				message: "The error is" + err.message
-			}
-		}))
-		.pipe(useref())
-		.pipe(gulpIf("*.js", uglify()))
-		.pipe(notify())
-		.pipe(gulp.dest("build/"))
-});
-
-gulp.task("compress:images", function() {
-	return gulp.src("source/img/**/*.*")
-		.pipe(newer("build/img"))
-		.pipe(imagemin({
-			progressive: true
-		}))
-		.pipe(debug({
-			title: "Images are compressed: "
-		}))
-		.pipe(gulp.dest("build/img"))
-});
-
-gulp.task("copy:favicon", function() {
-	return gulp.src("source/favicon.ico")
-		.pipe(gulp.dest("build/"))
-});
-
-gulp.task("copy:fonts", function() {
-	return gulp.src("source/fonts/**")
-		.pipe(newer("build/fonts"))
-		.pipe(debug({
-			title: "Fonts are copied: "
-		}))
-		.pipe(gulp.dest("build/fonts"))
-});
-
+// Build
 gulp.task("build", gulp.series(
 	"clean",
-	gulp.parallel("compile:styles", "compile:views", "copy:favicon", "compress:images", "copy:fonts"))
+	gulp.parallel("compile:views", "compile:styles", "copy:favicon", "compress:images", "copy:fonts"))
 );
 
+// Watcher
 gulp.task("watch", function() {
-	gulp.watch("source/sass/**/*.*", gulp.series("compile:styles"));
-	gulp.watch("source/img/**/*.*", gulp.series("compress:images"));
-	gulp.watch("source/fonts/**/*.*", gulp.series("copy:fonts"));
-	gulp.watch("source/pug/**/*.*", gulp.series("compile:views"));
+	global.watch = true;
+
+	gulp.watch(config.paths.watch.styles, gulp.series("compile:styles"));
+	gulp.watch(config.paths.watch.image, gulp.series("compress:images"));
+	gulp.watch(config.paths.watch.fonts, gulp.series("copy:fonts"));
+	gulp.watch(config.paths.watch.views, gulp.series("compile:views"))
+		.on('all', (event, filepath) => {
+			global.emittyChangedFile = filepath;
+		});
+	gulp.watch(config.paths.watch.scripts, gulp.series("scripts"));
 });
 
-gulp.task("serve", function() {
-	bs.init({
-		server: "build"
-	})
-
-	bs.watch("build/**/*.*").on("change", bs.reload)
-});
-
+// RUN THIS TASK AS A DEFAULT
 gulp.task("dev",
 	gulp.series("build", gulp.parallel("watch", "serve"))
 );
